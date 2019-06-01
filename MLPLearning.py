53a474fbebebdd7ce8d7bd98f710476918ae097c
CzyzuM <44680508+CzyzuM@users.noreply.github.com>
2019-06-01 19:33:33 +0200

    Added SGD

--- MLPLearning.py
+++ MLPLearning.py
@@ -56,29 +56,70 @@ def layer_response(prevLayerValues, weights):
     """ 
     return sigmoid(weights@prevLayerValues)
 
-def SumOfErrors(layerValues, expectedCategory):
-    expectedValues = np.zeros(shape=np.shape(layerValues))
-    expectedValues[expectedCategory] = 1
-    return np.sum(layerValues - expectedValues)
-
 def SSE(layerValues, expectedCategory):
     expectedValues = np.zeros(shape=np.shape(layerValues))
     expectedValues[expectedCategory] = 1
     errors = layerValues - expectedValues
     return np.sum(errors * errors)
 
-def predict_one_with_state(x, W_in_hidden, W_hidden_out):
+def MLP_response(x, W_in_hidden, W_hidden_out):
     """
-    Funkcja predykcji zwracająca wraz z wynikiem stan sieci 
-    w celu umożliwnienia przeprowadzenia propagacji wstecznej
+    Funkcja obliczająca stan każdej warstwy sieci po przerabianiu jednego przypadku testowego
+    :param x: - przypadek testowy - obrazek 28x28 w postaci wektora
+    :param W_in_hidden: - macierz wag połączeń między warstwą wejściową a warstwą ukrytą
+    :param W_hidden_out: - macierz wag połączeń między warstwą ukrytą a warstwą wyjściową
+    :return: krotka warstw sieci: (wejściowa, ukryta, wyjściowa)
     """
     hidden_layer_response = layer_response(x,W_in_hidden)
     output_layer_response = layer_response(hidden_layer_response,W_hidden_out)
-    prediction = np.argmax(output_layer_response)
-    return (prediction, hidden_layer_response, output_layer_response)
+    return (x, hidden_layer_response, output_layer_response)
+
+def gradient_descent_single_image(MLP_state, W_in_hidden, W_hidden_out, expectedCategory):
+    """
+    Funkcja obliczająca przybliżenie ujemnego gradientu SSE dla jednego przypadku testowego
+    :param MLP_state: obecny stan warstw: ukrytej oraz wyjściowej sieci
+    :param W_in_hidden: - macierz wag połączeń między warstwą wejściową a warstwą ukrytą
+    :param W_hidden_out: - macierz wag połączeń między warstwą ukrytą a warstwą wyjściową
+    :return: para macierzy będących wartościami o jakie trzeba zmienić wagi poszczególnych połączeń
+    """
+    (input, hidden, output) = MLP_state
+    W_in_hidden_changes = np.ones( shape = np.shape(W_in_hidden) )
+    W_hidden_out_changes = np.ones( shape = np.shape(W_hidden_out) )
+
+    for j in range(W_hidden_out_changes.shape[0]):
+        aj = output[j]
+        neuronError = 0
+        if(expectedCategory == j):
+            neuronError = aj - 1
+        W_hidden_out_changes[j] *= np.full(shape = W_hidden_out_changes.shape[1], fill_value = (neuronError * aj * (1-aj))) * hidden
+
+    for i in range((W_in_hidden_changes).shape[0]):
+        ai = hidden[i]
+        expectedValues = np.zeros(shape=np.shape(output))
+        expectedValues[expectedCategory] = 1
+        neuronErrors = output - expectedValues
+        sumOfHigherLevel = np.transpose(W_hidden_out)[i] @ (neuronErrors * output * (np.ones(shape=np.shape(output))-output))
+        W_in_hidden_changes[i] *= np.full(shape = W_in_hidden_changes[1], fill_value = (sumOfHigherLevel)*ai*(1-ai)) * input
+    return(W_in_hidden_changes,W_hidden_out_changes)
+
+def stochastic_gradient_descent( x_train, mini_batch_size, W_in_hidden, W_hidden_out ):
+    for i in range(np.shape(x_train)[0] / mini_batch_size):
+        batch = x_train[ mini_batch_size*i : mini_batch_size*(i+1)-1 ]
+        W_in_hidden_gradient_accumulator = np.zeros( shape = np.shape(W_in_hidden) )
+        W_hidden_out_gradient_accumulator = np.ones( shape = np.shape(W_hidden_out) )
+        for image in batch:
+            (a,b) = gradient_descent_single_image(MLP_response(image, W_in_hidden, W_hidden_out),W_in_hidden, W_hidden_out, expectedCategory)
+            W_in_hidden_gradient_accumulator += a
+            W_hidden_out_gradient_accumulator += b
+        W_in_hidden -= W_in_hidden_gradient_accumulator / mini_batch_size
+        W_hidden_out-= W_hidden_out_gradient_accumulator / mini_batch_size
 
 #main:
 X = preprocessing(x)
+x_train_test = X[:1000]
+#x_val = X[55000:]
+
+#print(x_train.shape)
+#print(x_val.shape)
 #W_in_hidden = initLayerWeights(28,X.shape[1])
 #W_hidden_out = initLayerWeights(10,28)
-
